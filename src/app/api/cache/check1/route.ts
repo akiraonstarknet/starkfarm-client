821d97254abdfc8808c05a492e4315694cf5b6ae
akiraonstarknet <zkdice@proton.me>
2024-09-26 16:18:48 +0530

    cache check apis [2]

--- src/app/api/cache/check1/route.ts
+++ src/app/api/cache/check1/route.ts
@@ -2,9 +2,12 @@ import { NextResponse } from 'next/server';
 
 export const revalidate = 1800;
 
-export async function GET(_req: Request) {
+export async function GET(_req: Request, res: Response) {
+  const data = await fetch('https://app.strkfarm.xyz/api/stats');
+  const json = await data.json();
   const response = NextResponse.json({
     time: new Date(),
+    data: json,
   });
 
   response.headers.set(
--- src/app/api/cache/check2/route.ts
+++ src/app/api/cache/check2/route.ts
@@ -3,8 +3,12 @@ import { NextResponse } from 'next/server';
 export const revalidate = 1800;
 
 export async function GET(_req: Request) {
+  const data = await fetch('https://app.strkfarm.xyz/api/stats');
+  const json = await data.json();
+
   const response = NextResponse.json({
     time: new Date(),
+    data: json,
   });
   return response;
 }
