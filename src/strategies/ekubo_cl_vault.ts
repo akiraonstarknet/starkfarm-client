b2d8558d2a70894f8dc6875de88dec3dd1c8a360
akiraonstarknet <zkdice@proton.me>
2025-10-14 11:12:04 +0800

    update Ekubo vaults APY to 7d

--- src/strategies/ekubo_cl_vault.ts
+++ src/strategies/ekubo_cl_vault.ts
@@ -276,7 +276,11 @@ export class EkuboClStrategy extends IStrategy<CLVaultStrategySettings> {
     const blocksDiff = this.metadata.additionalInfo.lstContract
       ? 600000
       : 600000 / 4;
-    const yieldInfo = await this.clVault.netAPY('latest', blocksDiff);
+    const yieldInfo = await this.clVault.netAPY(
+      'latest',
+      blocksDiff,
+      '7d' as any,
+    );
     this.netYield = yieldInfo;
     this.leverage = 1;
 
