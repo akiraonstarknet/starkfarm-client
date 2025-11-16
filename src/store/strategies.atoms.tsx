56d2da0f3e445d4f94036bb34291e17a692f9e63
akiraonstarknet <zkdice@proton.me>
2025-11-16 22:22:40 +0530

    bump xSTRK hyper limit to 3M

--- src/store/strategies.atoms.tsx
+++ src/store/strategies.atoms.tsx
@@ -412,7 +412,7 @@ export function getStrategies() {
     xLBTC: 5,
     xtBTC: 5,
     xsBTC: 5,
-    xSTRK: 550000,
+    xSTRK: 3000000,
   };
 
   const hyperLSTStrategies = HyperLSTStrategies.map((hyper) => {
